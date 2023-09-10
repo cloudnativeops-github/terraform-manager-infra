965ecde589d88439bdad2e031959b182adb3eda5
sa <sajin.net@gmail.com>
2023-09-10 08:50:02 +0200

    added terratest

--- terratest/tf_test.go
+++ terratest/tf_test.go
@@ -2,39 +2,46 @@ package test
 
 import (
     "testing"
-    "github.com/gruntwork-io/terratest/modules/aws"
-    "github.com/gruntwork-io/terratest/modules/terraform"
+    "os"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sts"
     "github.com/stretchr/testify/assert"
 )
 
-func TestTerraformRegionAndAccountID(t *testing.T) {
-    t.Parallel()
-
-    // Set up options for Terraform initialization and deployment
-    terraformOptions := &terraform.Options{
-        // Specify the path to your Terraform code
-        TerraformDir: "../",
+func TestAWSAccountID(t *testing.T) {
+    // Create an AWS session using your AWS credentials.
+    sess, err := session.NewSession()
+    if err != nil {
+        t.Fatalf("Failed to create AWS session: %v", err)
     }
 
-    // Defer the cleanup of resources until the test is complete
-    defer terraform.Destroy(t, terraformOptions)
+    // Create an STS (Security Token Service) client to retrieve the account ID.
+    svc := sts.New(sess)
 
-    // Initialize and apply the Terraform configuration
-    terraform.InitAndApply(t, terraformOptions)
+    // Use the GetCallerIdentity API to get information about the calling account.
+    identity, err := svc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+    if err != nil {
+        t.Fatalf("Failed to get caller identity: %v", err)
+    }
 
-    // Retrieve the AWS region and account ID
-    //region := aws.GetRegion(t)
-    accountID := aws.GetAccountId(t)
-	//vpcID := terraform.Output(t, terraformOptions, "vpc_id")
+    // Extract the AWS account ID from the caller identity.
+    accountID := *identity.Account
 
-    // Check if the region matches the expected region
-    //expectedRegion := "us-west-2"  // Set your expected AWS region
-    //assert.Equal(t, expectedRegion, region)
+    // Specify the expected AWS account ID.
+    expectedAccountID := "377308807353" // Replace with your actual AWS account ID.
 
-    // Check if the account ID matches the expected account ID
-    expectedAccountID := "377308807353"  // Set your expected AWS account ID
-    assert.Equal(t, expectedAccountID, accountID)
+    // Check if the retrieved AWS account ID matches the expected value.
+    assert.Equal(t, expectedAccountID, accountID, "AWS account ID does not match the expected value")
+}
 
-	//vpc := aws.GetVpcById(t, vpcID)
-	//assert.Equal(t, "10.0.0.0/16", vpc.CidrBlock)
+func TestMain(m *testing.M) {
+    // Set up any necessary AWS credentials and configuration here.
+
+    // Run the tests.
+    exitCode := m.Run()
+
+    // Clean up or perform any necessary teardown here.
+
+    // Exit with the appropriate exit code.
+    os.Exit(exitCode)
 }
